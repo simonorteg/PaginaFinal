f4ceec752d8ad501f60f29ccbd6ddbbcf9e498b0
simonorteg <saimonortega-j@hotmail.com>
2022-05-16 19:51:29 -0300

    asuste agregar img y direc

---
+++ validacorreo.js
@@ -0,0 +1,17 @@
+function validateEmailAddress(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+    }
+    function validate() {
+    $("#mail").text("");
+    var emailaddress = $("#email").val();
+    if (validateEmailAddress(emailaddress)) {
+    $("#mail").text(emailaddress + " es valido :)");
+    $("#mail").css("color", "green");
+    } else {
+    $("#mail").text(emailaddress + " no es correcto, por favor vuelva a ingresarlo:(");
+    $("#mail").css("color", "red");
+    }
+    return false;
+    }
+    $("#validate").bind("click", validate);
