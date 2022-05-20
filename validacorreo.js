6f59c763fec756be178c96ca5fcdbb5a99efcce9
simonorteg <saimonortega-j@hotmail.com>
2022-05-20 00:04:38 -0300

    ajuste

--- validacorreo.js
+++ validacorreo.js
@@ -2,6 +2,7 @@ function validateEmailAddress(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
     }
+    
     function validate() {
     $("#mail").text("");
     var emailaddress = $("#email").val();
